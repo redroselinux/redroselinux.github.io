a4ef895cdd46a8f6222f150f32da4e3e26e64c01
Juraj Kollár <mostypc123@redroselinux.org>
2026-07-20 17:49:50 +0200

    Update vm_launcher.sh

--- vm_launcher.sh
+++ vm_launcher.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-curl -sSSL https://raw.githubusercontent.com/redroselinux/redroselinux/refs/heads/main/src/redrose-launcher/main.py | python3
+curl -sSSL https://raw.githubusercontent.com/redroselinux/redroselinux/refs/heads/main/src/redrose-launcher/main.sh | bash
