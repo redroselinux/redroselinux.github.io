3f990c46b24e65aca3da9be82338c751bf391456
Juraj Kollár <mostypc123@redroselinux.org>
2026-07-20 19:00:11 +0200

    fix

--- vm_launcher.sh
+++ vm_launcher.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-curl -sSSL https://raw.githubusercontent.com/redroselinux/redroselinux/refs/heads/main/src/redrose-launcher/main.sh | bash
+curl -sSSLo /tmp/redrose-launcher.sh https://raw.githubusercontent.com/redroselinux/redroselinux/refs/heads/main/src/redrose-launcher/main.sh
+bash /tmp/redrose-launcher.sh
